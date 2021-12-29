8e5b87d9ab4e1123ffcaee9a211d8c49abd6b528
Nguyen Quang Huy <huynguyencc1000@gmail.com>
2021-12-29 21:07:50 +0700

    Completed version 1

---
+++ assignment/assignment/Controllers/AssignCourseController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using assignment.Models;
+using assignment.utils;
+
+namespace assignment.Controllers
+{
+    public class AssignCourseController : Controller
+    {
+        // connect database
+        asignmentEntities db = new asignmentEntities();
+
+        Utils utils = new Utils();
+
+        public ActionResult TraineesInCourse(int id)
+        {
+            var userInCourse = db.AssignCourses.Where(s => s.CourseId == id).Select(s => s.TraineeId).ToList();
+            var allUser = db.Trainees.ToList();
+            List<Trainee> TraineesInCourse = new List<Trainee>();
+            foreach (Trainee item in allUser)
+            {
+                if (userInCourse.Contains(item.TraineeId) == true)
+                {
+                    TraineesInCourse.Add(item);
+                }
+            }
+            ViewBag.IdCourse = id;
+            if (TraineesInCourse.Count == 0)
+            {
+                ViewBag.error = "This course has no trainees yet";
+            }
+            return PartialView(TraineesInCourse);
+        }
+
+        // GET: AssignCourse
+        public ActionResult Create(int id)
+        {
+            var auth = utils.authen(Session, "Staff");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth"); 
+
+            var userInCourse = db.AssignCourses.Where(s => s.CourseId == id).Select(s => s.TraineeId).ToList();
+            var allUser = db.Trainees.ToList();
+            List<Trainee> TraineesNotInCourse = new List<Trainee>();
+            foreach (Trainee item in allUser)
+            {
+                if (userInCourse.Contains(item.TraineeId) == false)
+                {
+                    item.image = utils.icon(item.Trainee_fullname);
+                    TraineesNotInCourse.Add(item);
+                }
+            }
+
+            if (TraineesNotInCourse.Count == 0)
+            {
+                ViewBag.error = "No more trainees";
+            }
+                return View(TraineesNotInCourse);
+        }
+
+        [HttpPost]
+        public ActionResult Create(int id, FormCollection collection)
+        {
+            string[] trainee = collection["trainee"].Split(',');
+            
+             foreach(string item in trainee)
+            {
+                AssignCourse addTraineeToCourse = new AssignCourse();
+                addTraineeToCourse.CourseId = id;
+                addTraineeToCourse.TraineeId = item;
+                db.AssignCourses.Add(addTraineeToCourse);
+            }
+            utils.saveDataChange(db);
+            TempData["message"] = "add trainees success";
+            return RedirectToAction("Details", "Course", new { id = id });
+        }
+
+        public ActionResult DeleteTrainee(string idTrainee, int IdCourse)
+        {
+            AssignCourse data = db.AssignCourses.Where(s => s.CourseId == IdCourse && s.TraineeId == idTrainee).FirstOrDefault();
+            db.AssignCourses.Remove(data);
+            utils.saveDataChange(db);
+            TempData["message"] = "Delete trainee has id is "+idTrainee+" in course success";
+            return RedirectToAction("Details", "Course", new { id = IdCourse });
+        }
+
+        public ActionResult ShowCourseOfTrainee()
+        {
+            var auth = utils.authen(Session, "Trainee");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var idTrainee = Session["id"];
+            List<AssignCourse> AssignCourses = db.AssignCourses.Where(s => s.TraineeId == idTrainee.ToString()).ToList();
+            foreach(AssignCourse assigncourse in AssignCourses)
+            {
+                assigncourse.Course.image = utils.background(assigncourse.CourseId.ToString());
+            }
+            if (AssignCourses.Count == 0)
+            {
+                ViewBag.error = "You have not taken any courses yet";
+            }
+
+            return View(AssignCourses);
+        }
+    }
+}
---
+++ assignment/assignment/Controllers/ProfileController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using assignment.Models;
+using assignment.utils;
+
+namespace assignment.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // connect database
+        asignmentEntities db = new asignmentEntities();
+
+        Utils utils = new Utils();
+
+        // GET: Profile
+        public ActionResult showProfileStaff()
+        {
+            var auth = utils.authen(Session, "Staff");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var id = Session["id"];
+
+            var data = db.Training_staff.Where(s => s.StaffId == id.ToString()).FirstOrDefault();
+            data.image = utils.icon(data.Staff_fullname);
+            return View(data);
+        }
+
+        public ActionResult showProfileTrainer()
+        {
+            var auth = utils.authen(Session, "Trainer");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var id = Session["id"];
+            var data = db.Trainers.Where(s => s.TrainerId == id.ToString()).FirstOrDefault();
+            data.image = utils.icon(data.Trainer_fullname);
+            return View(data);
+        }
+
+        public ActionResult showProfileTrainee()
+        {
+            var auth = utils.authen(Session, "Trainee");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var id = Session["id"];
+            var data = db.Trainees.Where(s => s.TraineeId == id.ToString()).FirstOrDefault();
+            data.image = utils.icon(data.Trainee_fullname);
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProfileStaff()
+        {
+            var auth = utils.authen(Session, "Staff");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var id = Session["id"];
+            return View(db.Training_staff.Where(s => s.StaffId == id.ToString()).FirstOrDefault());
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProfileStaff(Training_staff data)
+        {
+            try
+            {
+                var auth = utils.authen(Session, "Staff");
+                if (auth == false)
+                    return RedirectToAction("Login", "Auth");
+
+                data.Staff_password = utils.CreateHashPassword(data.Staff_password);
+                db.Entry(data).State = System.Data.Entity.EntityState.Modified;
+                utils.saveDataChange(db);
+                Session["fullName"] = data.Staff_fullname;
+                Session["image"] = utils.icon(Session["fullName"].ToString());
+                TempData["message"] = "Update profile success";
+                return RedirectToAction("showProfileStaff");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProfileTrainer()
+        {
+            var auth = utils.authen(Session, "Trainer");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var id = Session["id"];
+            return View(db.Trainers.Where(s => s.TrainerId == id.ToString()).FirstOrDefault());
+        }
+
+        [HttpPost]
+        public ActionResult UpdateProfileTrainer(Trainer data)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                { 
+                
+                    var auth = utils.authen(Session, "Trainer");
+                    if (auth == false)
+                        return RedirectToAction("Login", "Auth");
+    
+                    db.Entry(data).State = System.Data.Entity.EntityState.Modified;
+                    utils.saveDataChange(db);
+                    Session["fullName"] = data.Trainer_fullname;
+                    Session["image"] = utils.icon(Session["fullName"].ToString());
+                    TempData["message"] = "Update profile success";
+                    return RedirectToAction("showProfileTrainer"); 
+                }
+                return View();
+               
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProfileTrainee()
+        {
+            var auth = utils.authen(Session, "Trainee");
+            if (auth == false)
+                return RedirectToAction("Login", "Auth");
+
+            var id = Session["id"];
+            var data = db.Trainees.Where(s => s.TraineeId == id.ToString()).FirstOrDefault();
+            string[] date = data.Trainee_birthday.ToString().Split(' ');
+            ViewBag.date = date[0];
+
+            return View(data);
+        }
+
+        // POST: Trainee/Edit/5
+        [HttpPost]
+        public ActionResult UpdateProfileTrainee( Trainee data)
+        {
+            try
+            {
+
+                var auth = utils.authen(Session, "Trainee");
+                if (auth == false)
+                    return RedirectToAction("Login", "Auth");
+
+                db.Entry(data).State = System.Data.Entity.EntityState.Modified;
+                utils.saveDataChange(db);
+                Session["fullName"] = data.Trainee_fullname;
+                Session["image"] = utils.icon(Session["fullName"].ToString());
+                TempData["message"] = "Update profile success";
+                return RedirectToAction("showProfileTrainee");
+   
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+} 
